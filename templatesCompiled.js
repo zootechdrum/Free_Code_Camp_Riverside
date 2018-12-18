166e1a9ac2795f59ac18eb6958d6e69353933cf1
Cesar Gomez <zootechdrum@gmail.com>
2018-12-17 21:04:33 -0800

    added HandleBars script tage for precompilation

---
+++ templatesCompiled.js
@@ -0,0 +1,6 @@
+(function() {
+  var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
+templates['navBar'] = template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
+    return "";
+},"useData":true});
+})();
