37d822a3c89d19e50c65c644a2643a261b2a15a3
Cesar Gomez <zootechdrum@gmail.com>
2019-02-05 22:01:30 -0800

    Set up the structure for connection to mysql

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const express = require('express');
-const path = require('path')
+const mysql   = require('mysql')
+const path    = require('path')
 
 const app = express();
+
+const connection = mysql.createConnection({
+    host:'localhost',
+    user: 'zootechdrum',
+    database: "join_us"
+})
 
 app.use("/public", express.static(path.join(__dirname, 'public')));
 
