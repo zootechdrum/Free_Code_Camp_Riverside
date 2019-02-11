ca355a4f6a14e8438f4eeef3060fa61b6b513f76
Cesar Gomez <zootechdrum@gmail.com>
2019-02-11 10:41:00 -0800

    set up jawsdb template

--- app.js
+++ app.js
@@ -11,11 +11,10 @@ const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 
 
-const connection = mysql.createConnection({
-    host:'localhost',
-    user: 'zootechdrum',
-    database: "join_us"
-})
+const connection = mysql.createConnection(process.env.JAWSDB_URL)
+connection.connect()
+
+
 
 app.use("/public", express.static(path.join(__dirname, 'public')));
 
@@ -24,9 +23,19 @@ app.get('/', (req,res) => {
 })
 
 app.post('/register', function(req,res){
-    console.log("Post request sent to /register :email is " + req.body.email)
+    const person = {
+        email: req.body.email
+    };
+
+    connection.query('INSERT INTO userTable SET ?', person, function(err, result){
+        if(err){
+            throw error;
+        }
+        res.send("Thanks for joining our wait list!")
+    })
 })
 
+connection.end();
 
 app.listen(port, function(){
     console.log("Server running on 8080")
